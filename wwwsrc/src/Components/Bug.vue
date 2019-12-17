a133f7829187f2087cd62563c8064ce1ea5f4df5
Geneieve MacKenzie <mackenzie.gen@gmail.com>
2019-12-17 13:06:12 -0700

    Created first Vue

---
+++ wwwsrc/src/Components/Bug.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="Bug">
+
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Bug',
+    data(){
+        return {}
+    },
+    computed:{},
+    methods:{},
+    components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
